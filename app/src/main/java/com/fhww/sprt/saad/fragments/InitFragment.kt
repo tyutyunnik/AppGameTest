522dae9c664c62aadc822967d0bf17af94d73858
tyutyunnik <tyutyunnik.e@gmail.com>
2023-01-05 14:52:05 +0200

    ++

--- app/src/main/java/com/fhww/sprt/saad/fragments/InitFragment.kt
+++ app/src/main/java/com/fhww/sprt/saad/fragments/InitFragment.kt
@@ -1,10 +1,17 @@
 package com.fhww.sprt.saad.fragments
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.os.BatteryManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.fhww.sprt.saad.R
 import com.fhww.sprt.saad.databinding.FragmentInitBinding
 
@@ -45,4 +52,42 @@ class InitFragment : Fragment(R.layout.fragment_init) {
             }
         }
     }
+
+    private fun isDiceOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    private fun isBot(context: Context): Boolean {
+        val adb = Settings.Secure.getInt(
+            context.applicationContext.contentResolver,
+            Settings.Global.ADB_ENABLED, 0
+        ) != 0
+
+        val batteryManager =
+            context.applicationContext.getSystemService(AppCompatActivity.BATTERY_SERVICE) as BatteryManager
+        val batLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        val charging = context.applicationContext.registerReceiver(
+            null,
+            IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        )
+            ?.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0
+
+        return adb || (batLevel == 100 && charging)
+    }
+
+    private fun onlineInit() {
+        TODO("Not yet implemented")
+    }
+
+    private fun startDiceGame() {
+        findNavController().navigate(R.id.action_initFragment_to_gameMenuFragment)
+    }
+
+    private fun startShake() {
+        findNavController().navigate(R.id.action_initFragment_to_shakeFragment)
+    }
+
 }
